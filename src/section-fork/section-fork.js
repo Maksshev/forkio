52cb66d884ce3353e9dc9df74aeb3e59bfbf4fc4
Maksym Shevchenko <maksym.shevchenko@icloud.com>
2018-10-23 18:40:56 +0300

    Seems final

--- src/section-fork/section-fork.js
+++ src/section-fork/section-fork.js
@@ -1,10 +1,13 @@
 $($('.header__navbar-nav')[0]).click(function(e){
-		$($('.header__navbar-nav')[0]).toggleClass('active');
-		let header = $($('.header')[0])
-		if(header.css('overflow') == 'visible'){
-			header.css('overflow','hidden')
-		}
-		else{
-			header.css('overflow','visible')
-		}
-})
+    let windowWidth = $(window).width()
+    if(windowWidth <= 481){
+        $($('.header__navbar-nav')[0]).toggleClass('active');
+        let header = $($('.header')[0])
+        if(header.css('overflow') == 'visible'){
+            header.css('overflow','hidden')
+        }
+        else{
+            header.css('overflow','visible')
+        }
+    }
+});
