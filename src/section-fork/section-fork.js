186b363ab3ede1e22d6cf3278379fca5ded16cbb
Alexey Kushnarov <alexey.kushnarov@ukr.net>
2018-10-23 16:57:08 +0300

    toggle overflow

--- src/section-fork/section-fork.js
+++ src/section-fork/section-fork.js
@@ -1,6 +1,7 @@
 document.addEventListener('click',function(e) {
 	if(e.target.closest('.toggle-navbar')){
-		document.getElementsByClassName('header__navbar-nav')[0].classList.toggle('active')
+		document.getElementsByClassName('header__navbar-nav')[0].classList.toggle('active');
+		let header = document.getElementsByClassName('header')[0]
+		header.style.overflowX = header.style.overflowX === 'visible' ? 'hidden' : 'visible';
 	}
 })
-
