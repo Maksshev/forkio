dcae91a433c80b46e8b5f9b3c79fab91d8dec017
Maksym Shevchenko <maksym.shevchenko@icloud.com>
2018-10-23 13:50:16 +0300

    Gallery updated

--- src/section-social/section-social.js
+++ src/section-social/section-social.js
@@ -1,5 +1,6 @@
 const item = $('.carousel-item');
 const arrow = $('.arrow');
+let clickCounter = 0;
 
 for (let i = 0, l = item.length; i < l; i++) {
     $(item[i]).attr('id', `${i}`).css('position', 'relative');
@@ -10,35 +11,50 @@ $(item).hide();
 $('#2').show();
 
 arrow.click(function (e) {
-    let visible = $('.carousel-item:visible')[0];
-    let visibleWidth = $(visible).width();
-    let offsetTop = $(visible).offset().top;
-    let offsetLeft = $(visible).offset().left;
-    let idVisible = Number($(visible).attr('id'));
-    if ($(e.target).hasClass('arrow-left')) {
-        let showItem = $(`#${idVisible + 1}`);
-        if (idVisible + 1 === item.length) {
-            showItem = $(`#0`);
+    if (clickCounter === 0) {
+        clickCounter++;
+        let visible = $('.carousel-item:visible')[0];
+        let visibleWidth = $(visible).width();
+        let offsetTop = $(visible).offset().top;
+        let offsetLeft = $(visible).offset().left;
+        let idVisible = Number($(visible).attr('id'));
+        if ($(e.target).hasClass('arrow-left')) {
+            let showItem = $(`#${idVisible + 1}`);
+            if (idVisible + 1 === item.length) {
+                showItem = $(`#0`);
+            }
+            let offsetObj = {top: `${offsetTop}`, left: `${$(window).width()}`};
+            $(visible).css('position', 'absolute').offset({
+                top: offsetTop,
+                left: offsetLeft
+            }).width(visibleWidth).animate({left: `-=${Number(offsetObj.left) - Number(offsetLeft)}`}, 'slow', function () {
+                $(this).css({position: 'relative', width: 'auto'}).hide();
+                clickCounter = 0;
+            });
+            $(showItem).show();
+            $(showItem).offset(offsetObj);
+            $(showItem).animate({left: `-=${Number(offsetObj.left) - Number(offsetLeft)}`}, 'slow', function () {
+                $(this).css('position', 'static');
+            });
+        } else if ($(e.target).hasClass('arrow-right')) {
+            let showItem = $(`#${idVisible - 1}`);
+            if (idVisible - 1 === -1) {
+                showItem = $(`#${item.length - 1}`);
+            }
+            let offsetObj = {top: `${offsetTop}`, left: `${-(Number(offsetLeft) + visibleWidth)}`};
+            $(visible).css('position', 'absolute').offset({
+                top: offsetTop,
+                left: offsetLeft
+            }).width(visibleWidth).animate({left: `+=${-Number(offsetObj.left) + Number(offsetLeft)}`}, 'slow', function () {
+                $(this).css({position: 'relative', width: 'auto'}).hide();
+                clickCounter = 0;
+            });
+            $(showItem).show();
+            $(showItem).offset(offsetObj);
+            $(showItem).animate({left: `+=${-Number(offsetObj.left) + Number(offsetLeft)}`}, 'slow', function () {
+                $(this).css('position', 'static');
+            });
         }
-        $(visible).hide();
-        $(showItem).show();
-        let offsetObj = {top: `${offsetTop}`, left: `${$(window).width()}`};
-        $(showItem).offset(offsetObj);
-        $(showItem).animate({left: `-=${Number(offsetObj.left) - Number(offsetLeft)}`}, 'slow', function () {
-            $(this).css('position', 'static');
-        });
-    } else if ($(e.target).hasClass('arrow-right')) {
-        let showItem = $(`#${idVisible - 1}`);
-        if (idVisible - 1 === -1) {
-            showItem = $(`#${item.length-1}`);
-        }
-        $(visible).hide();
-        $(showItem).show();
-        let offsetObj = {top: `${offsetTop}`, left: `${-(Number(offsetLeft) + visibleWidth)}`};
-        $(showItem).offset(offsetObj);
-        $(showItem).animate({left: `+=${-Number(offsetObj.left) + Number(offsetLeft)}`}, 'slow', function () {
-            $(this).css('position', 'static');
-        });
     }
 });
 
